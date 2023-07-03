26309a744823de695dfba64a0f16d00f3cdf5561
JoaoSegolin <joe-ws@hotmail.com>
2023-07-03 10:46:01 -0300

    Create spring project using initializer

---
+++ apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/ApitesteconsultoriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitesteconsultoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitesteconsultoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitesteconsultoriaApplication.class, args);
+	}
+
+}
