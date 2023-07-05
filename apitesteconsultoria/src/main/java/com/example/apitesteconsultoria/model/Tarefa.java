8b89578a30fccff5d1844a65146316037ba496b2
JoaoSegolin <joe-ws@hotmail.com>
2023-07-04 23:06:27 -0300

    Add getter and setter methods to tarefa object

--- apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Tarefa.java
+++ apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Tarefa.java
@@ -19,4 +19,68 @@ public class Tarefa {
     private int duracao;
     private int id_pessoa;
     private Boolean finalizado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Timestamp getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(Timestamp prazo) {
+        this.prazo = prazo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public Boolean getFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(Boolean finalizado) {
+        this.finalizado = finalizado;
+    }
 }
