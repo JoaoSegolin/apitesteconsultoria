d1f421fadfee8ceef6b62ed1d0bad9bf119d34bb
JoaoSegolin <joe-ws@hotmail.com>
2023-07-04 23:25:28 -0300

    Alter return types to Optional for testing

--- apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/service/PessoaService.java
+++ apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/service/PessoaService.java
@@ -4,6 +4,7 @@ import com.example.apitesteconsultoria.model.Pessoa;
 import com.example.apitesteconsultoria.repository.PessoaRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -13,19 +14,19 @@ public class PessoaService {
 
     public PessoaService(PessoaRepository pessoaRepository) { this.pessoaRepository = pessoaRepository; }
 
-    public String cadastraPessoa(Pessoa pessoa) {
+    public Optional<Pessoa> cadastraPessoa(Pessoa pessoa) {
         pessoaRepository.save(pessoa);
-        return "Pessoa cadastrada";
+        return pessoaRepository.findById(pessoa.getId());
     }
 
-    public String alteraPessoa(Pessoa pessoa) {
-        Optional<Pessoa> pessoaTemp = pessoaRepository.findById(pessoa.getId());
+    public Optional<Pessoa> alteraPessoa(int id, Pessoa pessoa) {
+        Optional<Pessoa> pessoaTemp = pessoaRepository.findById(id);
         pessoaTemp.get().setNome(pessoa.getNome());
         pessoaTemp.get().setDepartamento(pessoa.getDepartamento());
         pessoaTemp.get().setId_tarefa(pessoa.getId_tarefa());
 
         pessoaRepository.save(pessoaTemp.get());
-        return "Pessoal alterada com sucesso";
+        return pessoaTemp;
     }
 
     public String removePessoa(int id) {
