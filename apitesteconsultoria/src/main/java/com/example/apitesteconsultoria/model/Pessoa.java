580a6bad70b6163a4f961828789cc56f51036b9c
JoaoSegolin <joe-ws@hotmail.com>
2023-07-04 22:21:29 -0300

    Create pessoa object

--- apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Pessoa.java
+++ apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Pessoa.java
@@ -1,2 +1,15 @@
-package com.example.apitesteconsultoria.model;public class Pessoa {
+package com.example.apitesteconsultoria.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String nome;
+    private String departamento;
 }
