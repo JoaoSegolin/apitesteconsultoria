28f98deb914fade9efbe366e6a47fecefbe2c8d9
JoaoSegolin <joe-ws@hotmail.com>
2023-07-04 22:22:24 -0300

    Create tarefa object

--- apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Tarefa.java
+++ apitesteconsultoria/src/main/java/com/example/apitesteconsultoria/model/Tarefa.java
@@ -1,2 +1,22 @@
-package com.example.apitesteconsultoria.model;public class Tarefa {
+package com.example.apitesteconsultoria.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String titulo;
+    private String descricao;
+    private Timestamp prazo;
+    private String departamento;
+    private int duracao;
+    private int id_pessoa;
+    private Boolean finalizado;
 }
